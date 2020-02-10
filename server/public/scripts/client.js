0137e5b8764ce58e43fb9e23dac6a1e65bdcfc46
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2020-02-10 10:01:54 -0600

    input captured

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,24 @@
 $( document ).ready( onReady );
 
+// globals
+let selectedOperator = '';
+
+function equals(){
+    console.log( 'in equals' );
+    let objectToSend = {
+        num0: $( '#num0In').val(),
+        num1: $( '#num1In').val(),
+        operator: selectedOperator
+    }
+    console.log( 'sending:', objectToSend );
+} // end equals
+
 function onReady(){
-    console.log( 'JQ' )
+    $( '#equalsButton' ).on( 'click', equals );
+    $( '.operatorButton' ).on( 'click', operator );
 } // end onReady
+
+function operator(){
+    console.log( 'in operator:', $( this ).text() );
+    selectedOperator = $( this ).text();
+} // end operator
